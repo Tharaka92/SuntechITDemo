10c36fc385ddd55ffe4300c07ac14925a6b4e1a8
Tharaka Herath <Tharaka92@users.noreply.github.com>
2023-09-07 13:53:20 +0530

    seeding customers

--- SuntechIT.Demo.Infrastructure/Helpers/DataSeeder.cs
+++ SuntechIT.Demo.Infrastructure/Helpers/DataSeeder.cs
@@ -1,16 +1,25 @@
 ﻿using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SuntechIT.Demo.Domain.Entities.Customers;
 using SuntechIT.Demo.Domain.Enums;
+using SuntechIT.Demo.Domain.Repositories;
 
 namespace SuntechIT.Demo.Infrastructure.Helpers
 {
     public class DataSeeder : IDataSeeder
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public DataSeeder(RoleManager<IdentityRole> roleManager)
+        public DataSeeder(RoleManager<IdentityRole> roleManager,
+             ApplicationDbContext dbContext,
+             IUnitOfWork unitOfWork)
         {
             _roleManager = Guard.Against.Null(roleManager);
+            _dbContext = Guard.Against.Null(dbContext);
+            _unitOfWork = Guard.Against.Null(unitOfWork);
         }
 
         public async Task SeedAsync()
@@ -18,6 +27,7 @@ namespace SuntechIT.Demo.Infrastructure.Helpers
             await CreateSuperAdminRoleAsync();
             await CreateAdminRoleAsync();
             await CreateNormalRoleAsync();
+            await CreateCustomersAsync();
         }
 
         #region Private Methods
@@ -43,6 +53,17 @@ namespace SuntechIT.Demo.Infrastructure.Helpers
                 return;
 
             await _roleManager.CreateAsync(new IdentityRole(Role.Normal.ToString()));
+        }
+
+        private async Task CreateCustomersAsync()
+        {
+            if (await _dbContext.Customers.AnyAsync())
+                return;
+
+            _dbContext.Customers.Add(new Customer("Tharaka"));
+            _dbContext.Customers.Add(new Customer("Lahiru"));
+
+            await _unitOfWork.SaveChangesAsync();
         }
 
         private async Task<bool> IsRoleExistsAsync(string role) => await _roleManager.RoleExistsAsync(role);
